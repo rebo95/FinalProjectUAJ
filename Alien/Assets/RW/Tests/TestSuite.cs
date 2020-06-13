baf60cfe5063104d4ce8bffb40e174f741ca188c
rebo95 <arebored@ucm.es>
2020-06-13 18:56:13 +0200

    Setting Up Test Runner, TestSuite and GameAssembly, ready to start writing tests

---
+++ Alien/Assets/RW/Tests/TestSuite.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+public class TestSuite
+{
+    private Game game;
+
+    // 1
+    [UnityTest]
+    public IEnumerator AsteroidsMoveDown()
+    {
+        GameObject gameGameObject =
+            MonoBehaviour.Instantiate(Resources.Load<GameObject>("Prefabs/Game"));
+        game = gameGameObject.GetComponent<Game>();
+        GameObject asteroid = game.GetSpawner().SpawnAsteroid();
+        float initialYPos = asteroid.transform.position.y;
+        
+        yield return new WaitForSeconds(0.1f);
+        Assert.Less(asteroid.transform.position.y, initialYPos);
+        Object.Destroy(game.gameObject);
+    }
+
+}
+
