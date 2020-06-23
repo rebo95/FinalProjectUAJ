f95cb5f4ef3dcb988c4b30de3b879d496e7110b5
Ramón <ramonarj@ucm.es>
2020-06-23 02:31:38 +0200

    Empezados los tests de Editor

---
+++ Alien/Assets/RW/Scripts/Editor/DemoWindow.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+
+//Ventana del editor para elegir parámetros sobre la música generativa
+public class DemoWindow : EditorWindow
+{
+    /*
+     * Inicializa la ventana y crea una entrada en el menú
+     */
+    [MenuItem("Window/Demo Window")]
+    static void Init()
+    {
+        DemoWindow window = (DemoWindow)EditorWindow.GetWindow(typeof(DemoWindow));
+        window.Show();
+    }
+
+    /*
+     * Se llama cada vez que se abre la ventana
+     */
+    private void Awake()
+    {
+    }
+
+    /*
+     * "Tick" de la ventana (para mostrar cosas al usuario)
+     */
+    void OnGUI()
+    {
+    }
+}
---
+++ Alien/Assets/Tests/EditMode/Editor_Test_Suite.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+using UnityEngine;
+using UnityEditor;
+
+
+//Suite de tests para el Editor
+public class Editor_Test_Suite 
+{
+    private DemoWindow window;
+
+    //Incializador
+    [SetUp]
+    public void Setup()
+    {
+        window = (DemoWindow)EditorWindow.GetWindow(typeof(DemoWindow));
+    }
+
+    //Destructor
+    [TearDown]
+    public void Teardown()
+    {
+
+    }
+
+    //Test nº1
+    [UnityTest]
+    public IEnumerator WindowCreated()
+    {
+        yield return new WaitForSeconds(0.1f);
+        Assert.True(window == null);
+    }
+}
